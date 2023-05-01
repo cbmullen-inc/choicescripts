2fb501f0be7b501f25375e2359e0cdc83c2d5c19
cbmullen <cbmullen@gmail.com>
2023-05-01 20:23:05 +0100

    use more complex game to test process

--- scripts/choicescript.sh
+++ scripts/choicescript.sh
@@ -14,9 +14,9 @@ for var in "${env_vars[@]}"; do
 done
 
 # Check locations.
-SRC_LOCATION="./choicescript_src"
-TEMP_LOCATION="${SRC_LOCATION}/web/mygame/scenes"
-GAME_LOCATION="./games/${GAME_NAME}/scenes"
+SRC_LOCATION="choicescript_src"
+TEMP_LOCATION="${SRC_LOCATION}/web/mygame"
+GAME_LOCATION="./games/${GAME_NAME}"
 
 dirs=("${GAME_LOCATION}" "${SRC_LOCATION}" "${TEMP_LOCATION}")
 
@@ -28,20 +28,28 @@ for dir in "${dirs[@]}"; do
 done
 
 # Copy your game into the choicescript_src, as it has all the boilerplate
-rm -r "${TEMP_LOCATION}"
-cp -r "${GAME_LOCATION}" "${TEMP_LOCATION}"
+cp -r "${GAME_LOCATION}"/scenes "${TEMP_LOCATION}"
+cp -r "${GAME_LOCATION}"/images "${TEMP_LOCATION}"
+cp -r "${GAME_LOCATION}"/sounds "${TEMP_LOCATION}"
 
 # Are we compiling?
 if [ "${ACTION}" == "compile" ]; then
     cd "${SRC_LOCATION}" && node compile "..\\games\\${GAME_NAME}\\index.html"
+    cd -
 fi
 
 # Are we running random tests?
 if [ "${ACTION}" == "randomtest" ]; then
     cd "${SRC_LOCATION}" && node randomtest
+    cd -
 fi
 
 # Are we running quick tests?
 if [ "${ACTION}" == "quicktest" ]; then
     cd "${SRC_LOCATION}" && node quicktest
+    cd -
 fi
+
+rm -r "${TEMP_LOCATION}/scenes/"
+rm -r "${TEMP_LOCATION}/images/"
+rm -r "${TEMP_LOCATION}/sounds/"
